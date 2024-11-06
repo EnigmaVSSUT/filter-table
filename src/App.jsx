b8ad3a6d16de234fcd69061915bc2ff81f27ccf1
Dharmendra Mahanta <157347956+dharmendra-007@users.noreply.github.com>
2024-11-06 22:28:38 +0530

    improved ui (#1)

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import './App.css'
 import Filter from './components/Filter'
 import ItemListPage from './components/ItemListPage'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'poppins, Arial, sans-serif',
+  },
+});
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Filter/>
       {/* <ItemListPage/> */}
-    </>
+    </ThemeProvider>
   )
 }
 
